24e7ff193d66962faee4e4bab82be6d412ad5ca1
Chaiwat Matarak <chaiwat.mat@gmail.com>
2019-10-30 23:09:35 +0700

    add unit test

--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -1,5 +1,6 @@
 using System;
 using Xunit;
+using HmacGenerator;
 
 namespace UnitTest
 {
@@ -8,7 +9,13 @@ namespace UnitTest
         [Fact]
         public void Test1()
         {
+            var expected = "e54a620a9fe0e096f9c4c56355b6cea4a9b38a48b66f537fed0e1d9ce68c08777caf7682cff6c00330a4674e7f23e11448b2795921512bd3e5879aff7163d080";
+            var input = "[{\"id\":\"10000\"},{\"id\":\"10001\"}]";
 
+            var auditor = new Hmac512Generator();
+            var result = auditor.GetSignature(input);
+
+            Assert.Equal(expected, result);
         }
     }
 }
